f0fd5991202bc5bc9067f1743df10debbf96cff0
Peter Barker <pbarker@barker.dropbear.id.au>
2023-01-18 16:55:18 +1100

    autotest: add test for HobbyWing/TMotor DataLink

--- Tools/autotest/arduplane.py
+++ Tools/autotest/arduplane.py
@@ -4915,10 +4915,11 @@ class AutoTestPlane(vehicle_test_suite.TestSuite):
         self.context_push()
 
         self.set_parameters({
-            "SERIAL5_PROTOCOL": 45,
+            "SERIAL5_PROTOCOL": 47,  # v3
             "RPM1_TYPE": 5,
             "RPM1_ESC_MASK": 4,
-            "SERVO_H_V3_MASK": 4,
+            "ESC_TLM_G1_PROT": 1,  # HobbyWing PRO v3
+            "ESC_TLM_G1_MASK": 4,  # ... on motor 3
             "SIM_ESC_TELEM": 0,
         })
         self.customise_SITL_commandline([
@@ -5349,6 +5350,71 @@ class AutoTestPlane(vehicle_test_suite.TestSuite):
         home = self.home_position_as_mav_location()
         self.assert_distance(home, adsb_vehicle_loc, 0, 10000)
 
+    def MinThrottle(self):
+        '''Make sure min throttle does not apply in manual mode and does in FBWA'''
+
+        servo_min = self.get_parameter("RC3_MIN")
+        servo_max = self.get_parameter("RC3_MAX")
+        min_throttle = 10
+        servo_min_throttle = servo_min + (servo_max - servo_min) * (min_throttle / 100)
+
+        # Set min throttle
+        self.set_parameter('THR_MIN', min_throttle)
+
+        # Should be 0 throttle while disarmed
+        self.change_mode("MANUAL")
+        self.drain_mav() # make sure we have the latest data before checking throttle output
+        self.assert_servo_channel_value(3, servo_min)
+
+        # Arm and check throttle is still 0 in manual
+        self.wait_ready_to_arm()
+        self.arm_vehicle()
+        self.drain_mav()
+        self.assert_servo_channel_value(3, servo_min)
+
+        # FBWA should apply throttle min
+        self.change_mode("FBWA")
+        self.drain_mav()
+        self.assert_servo_channel_value(3, servo_min_throttle)
+
+        # But not when disarmed
+        self.disarm_vehicle()
+        self.drain_mav()
+        self.assert_servo_channel_value(3, servo_min)
+
+    def HobbyWing_DataLink(self):
+        '''test support for DataLink devices'''
+        self.context_push()
+
+        self.set_parameters({
+            "SERIAL5_PROTOCOL": 46,
+            "RPM1_TYPE": 5,
+            "RPM1_ESC_MASK": 4,
+            "SIM_ESC_TELEM": 0,
+        })
+
+        self.customise_SITL_commandline([
+            "--uartF=sim:tmotordatalink:0,0,3",
+        ])
+
+        self.assert_received_message_field_values("RPM", {
+            "rpm1": 0,
+            "rpm2": -1,
+        })
+
+        self.wait_ready_to_arm()
+        self.arm_vehicle()
+        self.change_mode('TAKEOFF')
+
+        self.wait_message_field_values("RPM", {
+            "rpm1": 5000,
+        }, epsilon=100)
+
+        self.disarm_vehicle(force=True)
+
+        self.context_pop()
+        self.reboot_sitl()
+
     def tests(self):
         '''return list of all tests'''
         ret = super(AutoTestPlane, self).tests()
@@ -5430,6 +5496,7 @@ class AutoTestPlane(vehicle_test_suite.TestSuite):
             self.GlideSlopeThresh,
             self.HIGH_LATENCY2,
             self.HobbyWing_Platinum_PRO_v3,
+            self.HobbyWing_DataLink,
             self.MidAirDisarmDisallowed,
             self.AerobaticsScripting,
             self.MANUAL_CONTROL,
